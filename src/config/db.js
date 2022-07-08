d803c4ace62786305365b4629edf1da1b5d00afc
ManuelRM47 <manuelol2000@gmail.com>
2022-07-07 21:03:42 -0700

    Changed Mongoose version for pagination

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,15 @@ import mongoose from 'mongoose';
 
 dotenv.config();
 
-export const connectDB = async () => {
+export const connectDB = () => {
     try {
-        await mongoose.connect(process.env.ATLAS_URI);
+        mongoose.connect(process.env.ATLAS_URI, 
+        { 
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+            useCreateIndex: true 
+        });
         console.log('Connection to DB was successful');
     } catch (e) {
         console.error(e);
